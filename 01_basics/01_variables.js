ed2dc3817e67e43150803c36dcc7517d3ee085f3
Ankush Soni <44251970+stash-724@users.noreply.github.com>
2024-03-28 23:33:14 +0000

    variableAndTable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+var name = "ankush"
+let surname = "soni"
+const a = 10
+b = 100
+console.log(name);
+console.table([name, surname, a, b])
+/* 
+    1. var and let provides fuctionality of changing the value while cont values cannot be changed
+    2. Do not use var as it does not supports scopes of use 
+    3. we can randomly create variables without declaring var, let or const but it is a bad practice
+    4. variables without values store value as undefined
+*/
